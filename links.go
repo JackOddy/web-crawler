e1eee22d0dfa818b3bb272cc2de2612d41eb39c9
Jack Oddy <jack.oddy91@gmail.com>
2017-07-30 20:16:44 +0100

    :art: puts links logic in links.go and adds extraction logic onto the struct

--- links.go
+++ links.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"golang.org/x/net/html"
+	tags "golang.org/x/net/html/atom"
 	"strings"
 )
 
@@ -31,4 +34,32 @@ func (self Link) Valid() bool {
 		return false
 	}
 	return true
+}
+
+func (self *Link) Extract(tag html.Token) *Link {
+	for _, attr := range tag.Attr {
+		if attr.Key == "href" || attr.Key == "src" {
+			self.url = formatUrl(attr.Val)
+			break
+		}
+	}
+
+	if tag.DataAtom == tags.A {
+		self.isLink = true
+	}
+	return self
+}
+
+func formatUrl(url string) string {
+	var buffer bytes.Buffer
+
+	url = strings.TrimSpace(url)
+
+	if url != "" && string(url[0]) == "/" {
+		buffer.WriteString(*Domain)
+		buffer.WriteString(url)
+		return buffer.String()
+	}
+
+	return url
 }
--- scraper.go
+++ scraper.go
@@ -1,60 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"golang.org/x/net/html"
-	tags "golang.org/x/net/html/atom"
 	"net/http"
-	"strings"
 )
-
-func createLink(tag html.Token) Link {
-	var url string
-	for _, attr := range tag.Attr {
-		if attr.Key == "href" || attr.Key == "src" {
-			url = formatUrl(attr.Val)
-			break
-		}
-	}
-
-	if tag.DataAtom == tags.A {
-		return Link{url, true}
-	}
-	return Link{url, false}
-}
-
-func formatUrl(url string) string {
-	var buffer bytes.Buffer
-
-	url = strings.TrimSpace(url)
-
-	if url != "" && string(url[0]) == "/" {
-		buffer.WriteString(*Domain)
-		buffer.WriteString(url)
-		return buffer.String()
-	}
-
-	return url
-}
-
-func verifyTag(token html.Token) (link Link, ok bool) {
-	link = createLink(token)
-	if link.Valid() {
-		ok = true
-	}
-	return
-}
-
-func isOpeningOrSelfClosing(t html.Token) bool {
-	switch t.Type {
-	case html.StartTagToken:
-		return true
-	case html.SelfClosingTagToken:
-		return true
-	default:
-	}
-	return false
-}
 
 func Scrape(url string, resp *http.Response, found chan Link, pages chan Page) {
 	var assets, links []Link
@@ -85,4 +34,23 @@ func Scrape(url string, resp *http.Response, found chan Link, pages chan Page) {
 			}
 		}
 	}
+}
+
+func isOpeningOrSelfClosing(t html.Token) bool {
+	switch t.Type {
+	case html.StartTagToken:
+		return true
+	case html.SelfClosingTagToken:
+		return true
+	default:
+	}
+	return false
+}
+
+func verifyTag(token html.Token) (link Link, ok bool) {
+	link.Extract(token)
+	if link.Valid() {
+		ok = true
+	}
+	return
 }
