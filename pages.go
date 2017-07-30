bc610595112cbc64ccc49ea1266277ad84549dd1
Jack Oddy <jack.oddy91@gmail.com>
2017-07-30 18:38:57 +0100

    :seedling: adds a default reporting function to be replaced by tests to capture what would otherwise be sdout

--- pages.go
+++ pages.go
@@ -2,15 +2,19 @@ package main
 
 import "fmt"
 
+var Reporter = func(p Page) {
+	fmt.Printf("\n\n[%s] \n\tASSETS:\n\t%v\n\tLINKS:\n\t%v",
+		p.url,
+		p.assets,
+		p.links,
+	)
+}
+
 type Page struct {
 	url           string
 	assets, links []Link
 }
 
 func (self Page) Report() {
-	fmt.Printf("\n\n[%s] \n\tASSETS:\n\t%v\n\tLINKS:\n\t%v",
-		self.url,
-		self.assets,
-		self.links,
-	)
+	Reporter(self)
 }
