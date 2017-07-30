ab75b90d8fb5246f85cacdc66c96d10a4aa1612c
Jack Oddy <jack.oddy91@gmail.com>
2017-07-30 18:39:50 +0100

    :squirrel: tests for storing all page URLs in VisitedLinks - currently (correctly) is picking up external URLs and throwing an error

--- web-crawler_test.go
+++ web-crawler_test.go
@@ -7,7 +7,22 @@ import (
 )
 
 func TestWebCrawler(t *testing.T) {
+	var testPage Page
+	Reporter = func(p Page) {
+		testPage = p
+	}
+
 	os.Args = []string{"cmd", "-u=http://localhost:3000"}
 	defer TestServer(t)()
 	main()
+
+	for _, link := range testPage.links {
+		if _, ok := VisitedLinks.Load(link); !ok {
+			t.Errorf("Could not find %s in VisitedLinks", link.url)
+		}
+		t.Logf("Successfully crawled and stored %s", link.url)
+	}
+	// VisitedLinks.Range(func(key interface{}, value interface{}) bool {
+	// 	link := key.(Link)
+	// })
 }
